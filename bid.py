1c0ba948c74ca939a252cfdc760a4aad39a12c79
thehamzarocks <thehamzarocks@gmail.com>
2017-04-07 18:20:51 +0530

    adding sockets

--- bid.py
+++ bid.py
@@ -1,3 +1,17 @@
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 12345
+s.bind((host,port))
+
+s.listen(5)
+while True:
+	c, addr = s.accept()
+	print("Got connection from " + str(addr))
+	c.send('Thank you for connecting'.encode(encoding='utf_8'))
+	c.close()
+	
 class Player(object):
 	
 	def __init__(self,name,id):
